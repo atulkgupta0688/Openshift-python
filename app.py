78902a75c883b26c600eea5ad2d8b180972d33f2
atulkgupta0688 <60935937+atulkgupta0688@users.noreply.github.com>
2022-03-07 19:05:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
-
-app.run(host='0.0.0.0', port =5000)
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello World! Built with a Docker file.")
+    app.run(host='0.0.0.0', port =5000)
