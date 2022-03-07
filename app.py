cdad46715b206c5da5271d87cf28c226872116d1
atulkgupta0688 <60935937+atulkgupta0688@users.noreply.github.com>
2022-03-07 19:13:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
     return 'Hello, World!'
 if __name__ == '__main__':  # Script executed directly?
     print("Hello World! Built with a Docker file.")
-    app.run(host='0.0.0.0', port =5000)
+    app.run(host='0.0.0.0', port =5000 , debug = True)
